90b52feb8b843baf7d36f19bfc95ffb61485b483
sainthkh <sainthkh@naver.com>
2018-06-07 11:43:19 +0900

    Created navbar blade file and applied it to front-page, single, home.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.default')
+
+@section('content')
+<header class="header">
+    @include('layouts.navbar')
+    <div id="home" class="hero">
+        <div class="hero-container">
+            <div class="hero-message">
+                <h2 class="hero-title">Hi</h2>
+                <hr class="hero-hr">
+                <h4 class="hero-subhead"></h4>
+            </div>
+            <a class="hero-cta-btn" href="">
+                <i class="fas fa-gamepad"></i>
+                <span></span>
+            </a>
+        </div>
+    </div>
+</header>
+@endsection
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -2,26 +2,7 @@
 
 @section('content')
 <header class="header">
-    <nav class="topnav">
-        <div class="topnav-container">
-            @unless(has_custom_logo()) 
-                <a class="topnav-brand" rel="home" href="{{ home_url( '/' ) }}" title="{{ esc_attr( get_bloginfo( 'name', 'display' ) ) }}"><?php bloginfo( 'name' ); ?></a>
-            @else 
-                <?php the_custom_logo(); ?>
-            @endunless
-            
-            <button class="topnav-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="topnav-toggler-icon fa fa-bars"></span>
-            </button>
-            <div class="topnav-collapse" id="navbarTogglerDemo02">
-                <ul class="topnav-main-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ home_url( '/' ) }}/blog">Blog</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    @include('layouts.navbar')
 </header>
 <div id="app" class="content-area">
     <main id="main" class="site-main" role="main">
