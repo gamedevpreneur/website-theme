4f9b7261565d74a96a648078848f2fbb2e939819
sainthkh <sainthkh@naver.com>
2018-06-09 11:50:46 +0900

    Added sidebar and signup form.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,39 @@
 @section('content')
 <header class="header">
     @include('layouts.navbar')
-    <div id="home" class="hero">
-        <div class="hero-container">
-            <div class="hero-message">
-                <h2 class="hero-title">Hi</h2>
-                <hr class="hero-hr">
-                <h4 class="hero-subhead"></h4>
+    <div id="home" class="home-header">
+        <div class="home-signup-container">
+            <div class="home-signup-message">
+                <h2 class="home-signup-title">Sign up for exclusive game development tips.</h2>
             </div>
-            <a class="hero-cta-btn" href="">
-                <i class="fas fa-gamepad"></i>
-                <span></span>
-            </a>
+            <div class="home-signup-box">
+                <div class="home-signup-form-wrap">
+                    <form id="ck_subscribe_form" class="ck_subscribe_form main-signup-form" action="" data-remote="true">
+                        <input type="hidden" value="{&quot;form_style&quot;:&quot;minimal&quot;,&quot;embed_style&quot;:&quot;inline&quot;,&quot;embed_trigger&quot;:&quot;scroll_percentage&quot;,&quot;scroll_percentage&quot;:&quot;70&quot;,&quot;delay_seconds&quot;:&quot;10&quot;,&quot;display_position&quot;:&quot;br&quot;,&quot;display_devices&quot;:&quot;all&quot;,&quot;days_no_show&quot;:&quot;15&quot;,&quot;converted_behavior&quot;:&quot;show&quot;}" id="ck_form_options">       <input type="hidden" name="id" value="141553" id="landing_page_id">
+                        <input type="hidden" name="ck_form_recaptcha" value="" id="ck_form_recaptcha">
+                        <input type="text" name="name" class="form-control" id="ck_emailField" placeholder="your name" required />
+                        <input type="email" name="email" class="form-control" placeholder="your-best@email.com" required/>
+                        <button class="subscribe_button ck_subscribe_button btn-subscribe fields" id="ck_subscribe_button">         Subscribe       </button>       
+                    </form>
+                </div>
+            </div>
         </div>
+    </div>
+    <div class="entry-content">
+        <main class="posts">
+            @wpposts
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <h2 class="entry-title"><a href="{{ esc_url( get_permalink() ) }}" rel="bookmark">{{ get_the_title() }}</a></h2>
+                    <div class="entry-summary">
+                        <?php the_excerpt(); ?>
+                    </div>
+                    <a href="{{ esc_url( get_permalink() ) }}" class="btn">Continue Reading</a>
+                </article>
+            @endposts
+            @include('layouts.pagination')
+        </main>
+        <aside class="sidebar">
+        </aside>
     </div>
 </header>
 @endsection
